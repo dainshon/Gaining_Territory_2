e2fea2b9c3ac0435547fbf8b2a367e230225d35b
dainn <shon1107@naver.com>
2023-12-03 00:43:23 +0900

    modify best_selection

--- machine.py
+++ machine.py
@@ -69,34 +69,42 @@ class MACHINE():
                 if beta <= alpha:
                     break
             return best_value
+
     
     def find_best_selection(self): # depth 3 = 50초(1턴) 25초 (2턴) 14초(3턴), depth 4 = 9분쯤
-       self.increment_turn()
+        self.increment_turn()
+       
+       #available = [[point1, point2] for (point1, point2) in list(combinations(self.whole_points, 2)) if self.check_availability([point1, point2])]
+        available = self.valid_move()
+        num_available_line = len(available)
+        print("available 길이: ", num_available_line)
 
-       if self.num_turns < 3:
-            available = [[point1, point2] for (point1, point2) in list(combinations(self.whole_points, 2)) if self.check_availability([point1, point2])]
-            max_distance = 0 # 가장 멀리 그을 수 있는거 (단독/연결 고려 X)
-            max_index = 0  # 이때의 index
-            max_disconnected_index = -1  # 아무것도 연결 안되어있는것중에 긴 선분의 index
-            index_distance_dict = {}
+       #if self.num_turns < 3 and num_available_line:
+                       #print("available 길이: ", len(available))
+
+        max_distance = 0 # 가장 멀리 그을 수 있는거 (단독/연결 고려 X)
+        max_index = 0  # 이때의 index
+        max_disconnected_index = -1  # 아무것도 연결 안되어있는것중에 긴 선분의 index
+        index_distance_dict = {}
 
 
+        if(num_available_line > 4):
             # 0. 사각형 반가르는거면 ㄱㄱㄱㄱ0순위
             for i in range(len(available)):
                 line = available[i]
                 if(self.check_rectangle(line)):
                     # 무조건 ㄱㄱ
                     return line
-
+            
             # 1. 삼각형 만들 수 있으면 바로 ㄱ  
+            # 여러 삼각형 만들 수 있을때는...?
             for i in range(len(available)):
                 line = available[i]
-
+                flag = 1
                 if(self.check_triangle(line)):
                     return line
-                
 
-            # 2. 가장 길게 그을 수 있는거
+                # 2. 가장 길게 그을 수 있는거
                 distance = math.sqrt((line[0][0]-line[1][0])**2 + (line[0][1]-line[1][1])**2)
                 if(max_distance<distance):
                     max_distance = distance
@@ -105,10 +113,15 @@ class MACHINE():
                 # index_distance 정보 저장
                 index_distance_dict[i] = distance
             
-            # distance기준으로 내림차순 정렬
+                # distance기준으로 내림차순 정렬
             index_distance_dict = dict(sorted(index_distance_dict.items(), key=lambda item: item[1], reverse=True))
+        
 
+        # 사각형 만들 수 없다면 그때부터  rule/minmax 고민
 
+        flag = 0
+
+        if(num_available_line>20):
 
             # 길게 그을 수 있는 순으로 상대에게 기회 주지 않는 선 찾기
             # 3. 한 수 앞 예측
@@ -116,18 +129,20 @@ class MACHINE():
                 if(self.see_next_turn(available[idd], available)):
                     return available[idd]
 
-            # 4. 만들 수 있는 삼각형 없으면 제일 먼줄로 긋기
+                # 4. 만들 수 있는 삼각형 없으면 제일 먼줄로 긋기
             return available[max_index]
-    
-       else:
+
+        else:
             print("minmax")
             best_value = -math.inf
             best_selection = None
             alpha = -math.inf
             beta = math.inf
             best_score = -math.inf 
+
             for move in self.valid_move():
                 self.drawn_lines.append(move)
+                #value = self.min_max(num_available_line, alpha, beta, False)
                 value = self.min_max(3, alpha, beta, False) # False: opponent's turn
                 self.drawn_lines.remove(move)
 
@@ -197,7 +212,7 @@ class MACHINE():
                             continue
 
                         return False
-        #print("다음수에 상대가 할 거 없음!")           
+        print("다음수에 상대가 할 거 없음!")           
         return True  # t상대가 만들 수 있는 삼각형이 없음 -> 그어도됨
         
     def check_rectangle(self, line):
@@ -250,7 +265,7 @@ class MACHINE():
                     if(self.check_pointIntri(point1, point2, line1[0])):
                         return True
                 if(line1[1] in line2):
-                    print("3개의 선: ", line, line1, line2)
+                    #print("3개의 선: ", line, line1, line2)
                     if(self.check_pointIntri(point1, point2, line1[1])):
                         return True
         return False
